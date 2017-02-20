903fdcad472ce5dfb0008d0da531f055cd74515f
rodzod <chaclan.rodrigo@gmail.com>
2017-02-20 17:23:03 -0600

    first commit

--- src/scrabble/Scrabble.java
+++ src/scrabble/Scrabble.java
@@ -14,6 +14,7 @@ public class Scrabble {
     /**
      * @param args the command line arguments
      */
+    //prueba de commit
     public static void main(String[] args) {
         // TODO code application logic here
     }
